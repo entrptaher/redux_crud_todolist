16444eb614ce371c6f214e74676343b646d0e08b
NeuTrix <neutrix@brandgeneering.com>
2017-12-09 10:57:22 -0800

    clean render of todo page

--- redux_crud/src/components/Todo.js
+++ redux_crud/src/components/Todo.js
@@ -3,7 +3,17 @@ import React from 'react';
 const Todo = () => {
 
 return (
-	<h1> Howdy </h1>
+
+	<div>
+
+		<h1> My Todo List </h1>
+
+		<ul> 
+			<li>  to do item </li>
+		</ul>
+		
+	</div>
+
 	)
 }
 
