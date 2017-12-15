9ba71e4680f3b085e3f3b0d4acdbbe510451dc69
NeuTrix <neutrix@brandgeneering.com>
2017-12-15 01:16:25 -0800

    Add a date fn to set deafult due date to today

--- redux_crud/src/components/TodoForm.js
+++ redux_crud/src/components/TodoForm.js
@@ -15,8 +15,20 @@ const TodoForm = (props) => {
 		_task.focus();
 	}
 
+	const getDay = (date) => {
+  
+		let year = date.getFullYear();
+		let month = date.getMonth()+1;
+		let day = date.getDate();
+		let fullDate =[]
+		return [...fullDate,year,month,day].join("-")
+}
+
+	const _today = getDay(new Date())
+
 	return (
 		<div>
+			<h3> Todays Date:  {_today} </h3>
 			<h4> Add a new todo here: </h4>
 			<form onSubmit={handleSubmit}>
 
@@ -27,7 +39,7 @@ const TodoForm = (props) => {
 				</select>
 
 				<input ref= {(input) => _task = input} type = "text"  placeholder="Task" size={40} required></input>
-				<input ref= {(input) => _date = input} type = "date" defaultValue="2020-12-31" required></input>
+				<input ref= {(input) => _date = input} type = "date" defaultValue={_today} required></input>
 				
 				<input type="submit"/>
 				<input type = "reset"/>
@@ -39,13 +51,11 @@ const TodoForm = (props) => {
 TodoForm.propTypes = {
 	_task: PropTypes.string.isRequired,
 	_rank: PropTypes.string.isRequired,
-	_date: PropTypes.number.isRequired
 }
 
 TodoForm.defaultProps = {
 	_task: "",
 	_rank: "",
-	_date:2020-12-31,
 }
 
 export default TodoForm
