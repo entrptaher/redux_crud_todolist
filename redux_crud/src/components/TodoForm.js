2df7df224669b703d6b7186377801e238bdd6854
NeuTrix <neutrix@brandgeneering.com>
2017-12-15 00:26:03 -0800

    Updated the todo input fields with rank and date defaults

--- redux_crud/src/components/TodoForm.js
+++ redux_crud/src/components/TodoForm.js
@@ -2,15 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 // import { Form } from 'react-bootstrap'
 
-const handleClick = (event) => {
-	event.preventDefault()
-}
-
 const TodoForm = (props) => {
 
-	let _task, _rank
+	let _task, _rank, _date
 
-	const handleForm = (event) => {
+	const handleSubmit = (event) => {
 		event.preventDefault();
 		// alert("Hello")
 /*		if (_task.value == "" || _rank.value == "") {
@@ -19,22 +15,43 @@ const TodoForm = (props) => {
 */		props.addTodo(_task.value, _rank.value);
 		// reset the fields
 		_task.value ="";
-		_rank.value ="";
+		_date = 
 		_task.focus();
 	}
 
 	return (
 		<div>
 			<h4> Add a new todo here: </h4>
-			<form onSubmit={handleForm}>
-				<input ref= {(input) => _task = input} type = "text"  placeholder="Task" required></input>
-				<input ref= {(input) => _rank = input} type = "text" placeholder="Rank" required></input>
-			<button type="submit" > Add todo </button>
+			<form onSubmit={handleSubmit}>
+
+				<select ref= {(input) => _rank = input}>
+				  <option value="high">High</option>
+				  <option value="medium" defaultValue= "selected">Med</option>
+				  <option value="low">Low</option>
+				</select>
+
+				<input ref= {(input) => _task = input} type = "text"  placeholder="Task" size={60} required></input>
+				<input ref= {(input) => _date = input} type = "date" defaultValue="2020-12-31" required></input>
+				
+				<input type="submit"/>
+				<input type = "reset"/>
 			</form>
 		</div>
 	)
 }
 
 // add Proptype validation
+
+TodoForm.propTypes = {
+	_task: PropTypes.string.isRequired,
+	_rank: PropTypes.string.isRequired,
+	_date: PropTypes.number.isRequired
+}
+
+TodoForm.defaultProps = {
+	_task: "",
+	_rank: "",
+	_date:2020-12-31,
+}
 
 export default TodoForm
