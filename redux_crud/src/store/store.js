d09287707c96b7ee75863f8beb0e9eaf2f8084f3
NeuTrix <neutrix@brandgeneering.com>
2017-12-18 19:20:22 -0800

    Thottle saveState to limit calls to #stringify

--- redux_crud/src/store/store.js
+++ redux_crud/src/store/store.js
@@ -4,6 +4,7 @@ import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 import rootReducer from '../reducers';
 import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
 
 // create middlewares
 const middleware = applyMiddleware(promise(),thunk, logger);
@@ -14,12 +15,15 @@ const persistedState = loadState();
 const store = createStore(rootReducer, persistedState, middleware)
 
 // save the state anytime we have a change in the store
-store.subscribe(() => {
+// add lodash #throttle to prevent overuse of expensive
+// JSON #stringify method in the saveState fn.
+store.subscribe(throttle(() => {
 	saveState({ 
 		// pass specific object to limit scope of state saved
 		// e.g. not save state of a visibility filter
 		todos: store.getState().todos
 		});
-})
+	// limits the store update to just 1x second
+}, 1000))
 
 export default store;
