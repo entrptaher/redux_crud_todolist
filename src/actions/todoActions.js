7942bd401523e7b9d1e495d5d83d12ce743d0940
MICHAEL WALKER <michaelwalker@MICHAELs-MacBook-Pro.local>
2017-12-23 10:25:40 -0800

    linted actions

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,8 +1,8 @@
-import shortid from 'shortid'
+import shortid from 'shortid';
 
 export const addTodo = (_task, _rank, _date) => {
 	return {
-		type: "ADD_TODO",
+		type: 'ADD_TODO',
 		payload: {
 			id: shortid.generate(),
 			task: _task,
@@ -10,50 +10,50 @@ export const addTodo = (_task, _rank, _date) => {
 			date: _date,
 			complete: false
 		}
-	}
+	};
 };
 
 export const removeTodo = (_id) => {
 	return {
-		type: "REMOVE_TODO",
+		type: 'REMOVE_TODO',
 		payload: {id: _id}
-	}
+	};
 };
 
 export const toggleTodo = (_id) => {
 	return {
-		type: "TOGGLE_TODO",
+		type: 'TOGGLE_TODO',
 		payload: {id: _id}
-	}
+	};
 };
 
 export const updateTodo = (_id, _task) => {
 	return {
-		type: "UPDATE_TODO",
+		type: 'UPDATE_TODO',
 		payload: {
 			id: _id,
 			task: _task
 		}
-	}
+	};
 };
 
 export const updateRank = (_id, _rank) => {
 	return {
-		type: "UPDATE_RANK",
+		type: 'UPDATE_RANK',
 		payload: {
 			id: _id,
 			rank: _rank
 		}
-	}
+	};
 };
 
 export const updateDate = (_id, _date) => {
 	return {
-		type: "UPDATE_DATE",
+		type: 'UPDATE_DATE',
 		payload: {
 			id: _id,
 			date: _date
 		}
-	}
+	};
 };
 
