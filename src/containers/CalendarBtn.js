4ff140b3614518ed5011cc65c0d6d96db51e191e
MICHAEL WALKER <michaelwalker@MICHAELs-MacBook-Pro.local>
2017-12-23 11:56:13 -0800

    Linted CalendarBtn

--- src/containers/CalendarBtn.js
+++ src/containers/CalendarBtn.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form } from "react-bootstrap";
+import { Form } from 'react-bootstrap';
 
 //============================================================
 
 const PriorityBtn = (props) => {
 
-	let _date
+	let _date;
 	const handleChange = (event) => {
 		event.preventDefault();
-
 		props.updateDate(props.id, _date.value);
-		console.log("The new DATE is: ", _date.value)
-		return console.log("Task Date EDITED!")
-	}
+	};
 
 	return (
 		<Form >
 			<input 
-			type = "date"  
-			ref= { (input) => _date = input } 
-			onChange= { handleChange }
-			defaultValue = { props.currDate } required
+				type = 'date'  
+				ref= { (input) => _date = input } 
+				onChange= { handleChange }
+				defaultValue = { props.currDate } required
 			/>
 		</Form >
-	)
-} //end Container
+	);
+}; //end Container
 
 //============================================================
 
@@ -33,11 +30,11 @@ PriorityBtn.propTypes = {
 	updateDate: PropTypes.func.isRequired,
 	currDate: PropTypes.string,
 	id: PropTypes.string,
-}
+};
 
 PriorityBtn.defaultProps = {
 	updateDate: f => f,
-}
+};
 
 //============================================================
-export default PriorityBtn
+export default PriorityBtn;
