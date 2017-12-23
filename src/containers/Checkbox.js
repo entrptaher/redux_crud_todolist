2408cffc74896e8c3dac173c1dfeb6b2254ccc45
MICHAEL WALKER <michaelwalker@MICHAELs-MacBook-Pro.local>
2017-12-23 12:01:24 -0800

    Lint Checkbox

--- src/containers/Checkbox.js
+++ src/containers/Checkbox.js
@@ -1,40 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Checkbox } from "react-bootstrap";
+import { Form, Checkbox } from 'react-bootstrap';
 
 const _Checkbox = (props) => {
 
 	const handleClick = (event) => {
-		// event.preventDefault();
+		// disable event.preventDefault() to allow aninmation
+		event.preventDefault();
 		props.toggleTodo(props.id);
-		console.log("Task COMPLETED!")
-		console.log(event.target)
-	}
+	};
 	
 	if(props.complete) {
 		return( 
 			<Form >
 				<Checkbox 
-				className= { "form-check-input" }
-				type= "checkbox" 
-				onClick= { handleClick }
-				defaultChecked
+					className= { 'form-check-input' }
+					type= 'checkbox' 
+					onClick= { handleClick }
+					defaultChecked
 				> 
 				</Checkbox>
 			</Form>
-		)
+		);
 	} else {
 	
 		return (
 			<Form >
-					<Checkbox 
-					className= { "form-check-input" }
-					type= "checkbox" 
+				<Checkbox 
+					className= { 'form-check-input' }
+					type= 'checkbox' 
 					onClick= { handleClick }
-					> 
-					</Checkbox>
+				> 
+				</Checkbox>
 			</Form>
-		)
+		);
 	} // end return
 
 }; // end component
@@ -43,10 +42,10 @@ _Checkbox.propTypes = {
 	complete: PropTypes.bool.isRequired,
 	id: PropTypes.string,
 	toggleTodo: PropTypes.func.isRequired,
-}
+};
 
 _Checkbox.defaultProps = {
 	toggleTodo: f => f
-}
+};
 
-export default _Checkbox
+export default _Checkbox;
