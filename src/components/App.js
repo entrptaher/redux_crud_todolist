ea3c2bdb6796fbc4004d12038ecedb9d10acbc8f
MICHAEL WALKER <michaelwalker@MICHAELs-MacBook-Pro.local>
2017-12-23 10:56:30 -0800

    lint App.js but variable react errors persist

--- src/components/App.js
+++ src/components/App.js
@@ -2,109 +2,109 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import logo from '../assets/logo.svg';
 import './App.css';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 // ============ COMPONENTS ============ 
 // import { Button, Row, Col } from 'react-bootstrap'
-// bootstrap not working...
 import TodoForm from '../containers/TodoForm';
 import TodoList from '../containers/TodoList';
 
 // ============ FUNCTIONS ============ 
 import { 
-  addTodo, 
-  removeTodo,
-  toggleTodo,
-  updateDate,
-  updateTodo,
-  updateRank,
-  } from '../actions/todoActions'
+	addTodo, 
+	removeTodo,
+	toggleTodo,
+	updateDate,
+	updateTodo,
+	updateRank,
+} from '../actions/todoActions';
   
 // ===================================
 class App extends Component {
 
-  // constructor(props) {
-  //   super(props)
-  // }
+	// constructor(props) {
+	//   super(props)
+	// }
+	
+	render() {
 
-  render() {
+		return (
+			<div className="App">
+	
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">React/Redux Todo List</h1>
+				</header>
+				
+				<p> Todays Date: { Date() } </p>
+				
+				<TodoForm 
+					addTodo = { this.props.addTodo }
+				/>
 
-    return (
-      <div className="App">
+				<TodoList 
+					todoArray=  { this.props.todoArray }
+					removeTodo= { this.props.removeTodo }
+					toggleTodo= { this.props.toggleTodo }
+					updateDate= { this.props.updateDate }
+					updateRank= { this.props.updateRank }
+					updateTodo= { this.props.updateTodo }
+				/>
 
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React/Redux Todo List</h1>
-        </header>
-
-        <p> Todays Date: { Date() } </p>
-
-        <TodoForm 
-          addTodo = { this.props.addTodo }
-        />
-
-        <TodoList 
-          todoArray=  { this.props.todoArray }
-          removeTodo= { this.props.removeTodo }
-          toggleTodo= { this.props.toggleTodo }
-          updateDate= { this.props.updateDate }
-          updateRank= { this.props.updateRank }
-          updateTodo= { this.props.updateTodo }
-        />
-
-      </div>
-    );
-  }
-}; // end Class
+			</div>
+		);
+	}
+} // end Class
 
 // ===========================================================
 
 const mapStateToProps =(state) => {
-  return {
-    todoArray: state.todos
-  };
+	return {
+		todoArray: state.todos
+	};
 }; // end const
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    addTodo: (task, rank, date) => {
-      dispatch(addTodo(task, rank, date)) 
-    },
-    removeTodo: (id) => {
-      dispatch(removeTodo(id))
-    },
-    toggleTodo: (id) => {
-      dispatch(toggleTodo(id))
-    },
-    updateDate: (id, date) => {
-      dispatch(updateDate(id, date))
-    },
-    updateRank: (id, rank) => {
-      dispatch(updateRank(id,rank))
-    },
-    updateTodo: (id, task) => {
-      dispatch(updateTodo(id,task))
-    },
-  };
+	return {
+		addTodo: (task, rank, date) => {
+			dispatch(addTodo(task, rank, date));
+		},
+		removeTodo: (id) => {
+			dispatch(removeTodo(id));
+		},
+		toggleTodo: (id) => {
+			dispatch(toggleTodo(id));
+		},
+		updateDate: (id, date) => {
+			dispatch(updateDate(id, date));
+		},
+		updateRank: (id, rank) => {
+			dispatch(updateRank(id,rank));
+		},
+		updateTodo: (id, task) => {
+			dispatch(updateTodo(id,task));
+		},
+	};
 }; // end const
 
 // ===========================================================
 
 App.propTypes = { 
-  addTodo:    PropTypes.func.isRequired,
-  removeTodo: PropTypes.func.isRequired,
-  toggleTodo: PropTypes.func.isRequired,
-  updateDate: PropTypes.func.isRequired,
-  updateRank: PropTypes.func.isRequired,
-  updateTodo: PropTypes.func.isRequired,
-}
+	addTodo:    PropTypes.func.isRequired,
+	removeTodo: PropTypes.func.isRequired,
+	toggleTodo: PropTypes.func.isRequired,
+	updateDate: PropTypes.func.isRequired,
+	updateRank: PropTypes.func.isRequired,
+	updateTodo: PropTypes.func.isRequired,
+};
 
 App.defaultProps ={
-  addTodo:    f=>f,
-  removeTodo: f=>f,
-  toggleTodo: f=>f,
-  updateDate: f=>f,
-  updateRank: f=>f,
-  updateTodo: f=>f,
-}
+	addTodo: f=>f,
+	removeTodo: f=>f,
+	toggleTodo: f=>f,
+	updateDate: f=>f,
+	updateRank: f=>f,
+	updateTodo: f=>f,
+};
+
 export default connect(mapStateToProps, mapDispatchToProps)(App);
