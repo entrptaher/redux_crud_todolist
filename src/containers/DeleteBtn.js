0bc1d80e4f745e0d01bc3fe2abcf3d123d0e874d
MICHAEL WALKER <michaelwalker@MICHAELs-MacBook-Pro.local>
2017-12-23 13:07:00 -0800

    Linted

--- src/containers/DeleteBtn.js
+++ src/containers/DeleteBtn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from "react-bootstrap";
+import { Button } from 'react-bootstrap';
 
 const DeleteBtn = (props) => {
 
@@ -8,32 +8,30 @@ const DeleteBtn = (props) => {
 		event.preventDefault();
 		// allow restricted global use of `confirm`
 		//eslint-disable-next-line
-		let _confirmed = confirm("DELETE this item?") 
+		let _confirmed = confirm('DELETE this item?') 
 			
 		if (_confirmed) {
-			props.removeTodo(props.id);
-			return console.log("Task DELETED!")
+			return props.removeTodo(props.id);
 		} 
-		console.log( "DELETE Cancelled")
-	}
+	};
 
 	return (
 		<Button 
 			onClick= { handleClick } 
-			className= { "btn btn-danger btn-sm" }
+			className= { 'btn btn-danger btn-sm' }
 		> 
 			Delete 
 		</Button>
-	)
-}
+	);
+};
 
 DeleteBtn.propTypes = {
 	removeTodo: PropTypes.func.isRequired,
 	id: PropTypes.string
-}
+};
 
 DeleteBtn.defaultProps = {
 	removeTodo: f => f,
-}
+};
 
-export default DeleteBtn
+export default DeleteBtn;
