f065b203ce99f55c00cad91a9d2b4d2ae36a842e
MICHAEL WALKER <michaelwalker@MICHAELs-MacBook-Pro.local>
2017-12-23 11:43:28 -0800

    Adjust lint rc for react components and lin TodoForm

--- src/containers/TodoForm.js
+++ src/containers/TodoForm.js
@@ -4,49 +4,48 @@ import { Grid, Row, Col, Button, Form } from 'react-bootstrap';
 
 const TodoForm = (props) => {
 
-	 // create a default format for today's date
-    const getDay = (date) => {
-      let year = date.getFullYear();
-      let month = date.getMonth()+1;
-      let day = date.getDate();
-      let fullDate =[]
-      return [...fullDate, year, month, day].join("-")
-    }
-    const _today = getDay(new Date())
+	// create a default format for today's date
+	const getDay = (date) => {
+		let year = date.getFullYear();
+		let month = date.getMonth()+1;
+		let day = date.getDate();
+		let fullDate =[];
+		return [...fullDate, year, month, day].join('-');
+	};
+	const _today = getDay(new Date());
 
-	let _task, _rank, _date
+	let _task, _rank, _date;
 	
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		props.addTodo(_task.value, _rank.value, _date.value);
-		console.log("Task ADDED!")
 	
 		// reset the fields
-		_task.value ="";
+		_task.value ='';
 		_task.focus();
-	}
+	};
 
 	const style ={
-		// outline: "0.5px solid lightgrey",
+		// outline: '0.5px solid lightgrey',
 		padding:0,
-	}
+	};
 
 	const styleTop ={
-		// outline: "1px solid lightgrey",
+		// outline: '1px solid lightgrey',
 		// backgroundColor: "#ffdda3",//orange
 		// backgroundColor: "#60D9FA",
-		backgroundColor: "lightblue",
+		backgroundColor: 'lightblue',
 		borderRadius: 5,
 		padding:5,
 		paddingBottom:20,
 		marginTop: 20,
 		marginBottom: 20,
-	}
+	};
 
 	const styleTitle ={
-		color:"#006699",
+		color:'#006699',
 		marginBottom: 20,
-	}
+	};
 
 	return (
 		<Grid>
@@ -56,74 +55,74 @@ const TodoForm = (props) => {
 			
 				<Form onSubmit={ handleSubmit }>
 
-					<Col className="rank"  sm= {1} > 
+					<Col className='rank'  sm= {1} > 
 					</Col>
 				
-					<Col className="rank"  sm= {1} > 
+					<Col className='rank'  sm= {1} > 
 
 						<select  
 							ref= { (value) => _rank = value }
-							defaultValue="Med" 
+							defaultValue='Med' 
 						>
-						  <option value="High">High</option>
-						  <option value="Med" >Med</option>
-						  <option value="Low">Low</option>
+							<option value='High'>High</option>
+							<option value='Med' >Med</option>
+							<option value='Low'>Low</option>
 						</select>
 
 					</Col>
 
-					<Col className="date" style={ style } sm= {2} > 
+					<Col className='date' style={ style } sm= {2} > 
 						<input 
 							ref= { (input) => _date = input } 
-							type = "date"  
+							type = 'date'  
 							defaultValue = { _today } 
 							required
 						/> 
 					</Col>
 
-					<Col className="task"  sm= {6} > 
+					<Col className='task'  sm= {6} > 
 						<input 
 							ref= { (input) => _task = input } 
-							type = "text"  
-							placeholder="enter a new task here..." 
+							type = 'text'  
+							placeholder='enter a new task here...' 
 							size={ 60 } 
 							maxLength= {48} 
 							required
 						/>
 					</Col>
 
-					<Col className="add"   style= { style } sm= {1} > 
+					<Col className='add'   style= { style } sm= {1} > 
 						<Button 
-							className= { "btn btn-info btn-sm" } 
-							type= "submit" 
-							value= "Add" 
+							className= { 'btn btn-info btn-sm' } 
+							type= 'submit' 
+							value= 'Add' 
 						> Add </Button>
 					</Col>
 
-					<Col className="reset" style= { style } sm= {1} > 
+					<Col className='reset' style= { style } sm= {1} > 
 						<Button 
-							className= { "btn btn-warning btn-sm" } 
-							type = "reset"
-							> Reset </Button> 
+							className= { 'btn btn-warning btn-sm' } 
+							type = 'reset'
+						> Reset </Button> 
 					</Col>
 
 				</Form>
 
 			</Row>
 		</Grid>
-	)
-}
+	);
+};
 
 TodoForm.propTypes = {
 	_task: PropTypes.string.isRequired,
 	_rank: PropTypes.string.isRequired,
 	_date: PropTypes.string.isRequired,
-}
+};
 
 TodoForm.defaultProps = {
-	_task: "",
-	_rank: "",
-	_date: ""
-}
+	_task: '',
+	_rank: '',
+	_date: ''
+};
 
-export default TodoForm
+export default TodoForm;
