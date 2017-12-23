f094413e61ba236e4d7a5c6e1894eb4b9ee84a42
MICHAEL WALKER <michaelwalker@MICHAELs-MacBook-Pro.local>
2017-12-23 13:06:41 -0800

    Adjust linter for describe and it errors

--- src/tests/TodoReducer.test.js
+++ src/tests/TodoReducer.test.js
@@ -1,6 +1,11 @@
 // should split this into a reducer test suite
 // import React from 'react';
 
+// ======== esllint
+/*global TodoReducer describe:true*/
+/*global TodoReducer it:true*/
+/*eslint no-undef: "error"*/
+
 import deepFreeze from 'deep-freeze';
 import { expect } from 'chai'
 import store from '../store/store';
@@ -25,11 +30,11 @@ describe('The ADD_TODO function', () => {
 	})
 
 	it('...can ADD another task to that list', () => {
-		store.dispatch(addTodo("Mumby Corn Rocks!", "HIGH"))
+		store.dispatch(addTodo('Mumby Corn Rocks!', 'HIGH'))
 		let listAfter = store.getState().todos
 			expect(listAfter).to.be.an('array');
 			expect(listAfter.length).to.equal(listBefore.length+1)
-			expect(listAfter[3].task).to.equal("Mumby Corn Rocks!")
+			expect(listAfter[3].task).to.equal('Mumby Corn Rocks!')
 	})
 });
 
@@ -50,81 +55,81 @@ describe('The REMOVE_TODO function', () => {
 	})
 });
 
-describe("The TOGGLE_TODO function", () => {
+describe('The TOGGLE_TODO function', () => {
 
 	it('changes the todo complete status', () => {
-		let listB4 = store.getState().todos
-		expect(listB4).to.be.an("array")
+		let listB4 = store.getState().todos;
+		expect(listB4).to.be.an('array');
 		// ensure immutability
 		deepFreeze(listB4);
 
-		let _id = listB4[0].id
+		let _id = listB4[0].id;
 		expect(_id).to.be.a('string');
-		expect(_id).to.be.equal("0.1HxYz");
+		expect(_id).to.be.equal('0.1HxYz');
 
 		store.dispatch(toggleTodo(_id));
 
-		let listAFT = store.getState().todos
-		expect(listAFT[0].id).to.equal("0.1HxYz")
+		let listAFT = store.getState().todos;
+		expect(listAFT[0].id).to.equal('0.1HxYz');
 		let complete = listAFT[0].complete;
-		// console.log("*****this is the status:  ", complete, listB4[0].id);
+		// console.log('*****this is the status:  ', complete, listB4[0].id);
 
-			expect(complete).to.equal(true);
-	})
-})
+		expect(complete).to.equal(true);
+	});
+});
 
-describe("The UPDATE_TODO function", () => {
+describe('The UPDATE_TODO function', () => {
 	it('changes the todo content', () => {
-		let listB5 = store.getState().todos
-		expect(listB5).to.be.an("array")
+		let listB5 = store.getState().todos;
+		expect(listB5).to.be.an('array');
 		// ensure immutability
 		deepFreeze(listB5);
 
-		let _id = listB5[0].id
+		let _id = listB5[0].id;
 		expect(_id).to.be.a('string');
-		expect(_id).to.be.equal("0.1HxYz");
+		expect(_id).to.be.equal('0.1HxYz');
 
-		store.dispatch(updateTodo(_id,"Task Updated. Whatever!"));
+		store.dispatch(updateTodo(_id,'Task Updated. Whatever!'));
 
-		let listAFT2 = store.getState().todos
-		expect(listAFT2[0].task).to.equal("Task Updated. Whatever!")
-	})
-})
+		let listAFT2 = store.getState().todos;
+		expect(listAFT2[0].task).to.equal('Task Updated. Whatever!');
+	});
+});
 
-describe("The UPDATE_RANK function", () => {
+describe('The UPDATE_RANK function', () => {
 	it('changes the rank content', () => {
-		let listB5 = store.getState().todos
-		expect(listB5).to.be.an("array")
+		let listB5 = store.getState().todos;
+		expect(listB5).to.be.an('array');
 		// ensure immutability
 		deepFreeze(listB5);
 
-		let _id = listB5[0].id
+		let _id = listB5[0].id;
 		expect(_id).to.be.a('string');
-		expect(_id).to.be.equal("0.1HxYz");
+		expect(_id).to.be.equal('0.1HxYz');
 
-		store.dispatch(updateRank(_id,"Highest"));
+		store.dispatch(updateRank(_id,'Highest'));
 
-		let listAFT2 = store.getState().todos
-		expect(listAFT2[0].rank).to.equal("Highest")
+		let listAFT2 = store.getState().todos;
+		expect(listAFT2[0].rank).to.equal('Highest');
 
-	})
-})
+	});
+});
 
-describe.only("The UPDATE_DATE function", () => {
+describe.only('The UPDATE_DATE function', () => {
 	it('changes the DATE content', () => {
-		let listB5 = store.getState().todos
-		expect(listB5).to.be.an("array")
+		let listB5 = store.getState().todos;
+		expect(listB5).to.be.an('array');
 		// ensure immutability
 		deepFreeze(listB5);
 
-		let _id = listB5[0].id
+		let _id = listB5[0].id;
 		expect(_id).to.be.a('string');
-		expect(_id).to.be.equal("0.1HxYz");
+		expect(_id).to.be.equal('0.1HxYz');
 
-		store.dispatch(updateDate(_id,"Jan 25th"));
+		store.dispatch(updateDate(_id,'Jan 25th'));
 
-		let listAFT2 = store.getState().todos
-		expect(listAFT2[0].date).to.equal("Jan 25th")
+		let listAFT2 = store.getState().todos;
+		expect(listAFT2[0].date).to.equal('Jan 25th');
 
-	})
-})
+	});
+});
