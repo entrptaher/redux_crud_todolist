388b80cc5a35260de9d5caa8a4a705170782f53f
NeuTrix <neutrix@brandgeneering.com>
2017-12-26 22:15:14 -0800

    Add Nodemon

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/api', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send("Welcome to the React-Redux Todo experiment API")
+  res.send("Welcome to MY React-Redux Todo experiment API")
 });
 
 module.exports = router;
