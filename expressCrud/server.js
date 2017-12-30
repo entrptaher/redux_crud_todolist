eb3d25ecb9a8d8ad1fccfbe42000e49aee5ba160
NeuTrix <neutrix@brandgeneering.com>
2017-12-30 04:54:33 -0800

    add nodemon and clean up server

--- expressCrud/server.js
+++ expressCrud/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
-	console.log('Accessing server.js file');
+app.listen(3030, function() {
+	console.log('server listening on 3030');
+});
+
+// CRUD operations
+
+app.get('/', (req,res) => {
+	res.send('The GET path for READ...')
 });
