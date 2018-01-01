3f6bfd2d03f42b0f104026cb47eb207e5e84672c
NeuTrix <neutrix@brandgeneering.com>
2017-12-31 19:35:37 -0800

    set up server porject structure and config files for db

--- expressCrud/server.js
+++ expressCrud/server.js
@@ -1,24 +1,26 @@
 /*eslint no-console: "error"*/
 
-const express = require('express');
-const app = express();
+let express = require('express');
+let app = express();
+let mongoose = require('mongoose');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+let port = 8000;
+let todo = 
 
-const bodyParser = require('body-parser');
+
 // allows express to access form values
 app.use(bodyParser.urlencoded({extended: true}));
 //  allow express to utilize json
 app.use(bodyParser.json());
 
-// connect mongo db
-const MongoClient = require('mongodb').MongoClient
+// connect mongo db client
+// let MongoClient = require('mongodb').MongoClient;
 
-var db
-
-MongoClient.connect('mongodb://react-todo:services2015@ds135537.mlab.com:35537/react-redux-todo', (err, database) => {
+mongoose.connect('mongodb://react-todo:services2015@ds135537.mlab.com:35537/react-redux-todo', (err, database) => {
 
 	if(err) return console.log(err);
-
-	db = database.db('react-redux-todo');
+	// db = database.db('react-redux-todo');
 
 	// ... start the server
 	app.listen(3030, () => {
@@ -26,13 +28,18 @@ MongoClient.connect('mongodb://react-todo:services2015@ds135537.mlab.com:35537/r
 	});
 });
 
-// =========================== CRUD operations for todo items
+var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', () => {
+});
 
+// =========================== CRUD operations for todo items
 
 //  READ (Home)
 app.get('/', (req, res, err) => {
 	console.log("GET route")
-	res.sendFile(__dirname + '/index.html');*/
+	res.send("Got the home page")
+	// res.sendFile(__dirname + '/index.html');
 });
 
 //  READ (Todos- index)
@@ -89,23 +96,5 @@ app.delete('/todos/id', (req, res) => {
 });
 
 
-
 // https://zellwk.com/blog/crud-express-and-mongodb-2/
 
-/*  UPDATE
- app.put('/quotes', (req, res) => {
-  db.collection('quotes')
-  .findOneAndUpdate({name: 'Yoda'}, {
-    $set: {
-      name: req.body.name,
-      quote: req.body.quote
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-}) 
-*/
