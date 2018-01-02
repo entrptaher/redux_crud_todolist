e59829a304a49758eaee7a92b0a39c28aef48cf2
NeuTrix <neutrix@brandgeneering.com>
2018-01-02 15:45:26 -0800

    Still busted

--- expressCrud/test/todo.test.js
+++ expressCrud/test/todo.test.js
@@ -3,6 +3,9 @@
 let mongoose	= require('mongoose');
 let Todo = require('../controllers/models/todo.model');
 
+let request = require('supertest');
+// let express = require('express');
+
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server')
@@ -19,12 +22,17 @@ describe('Todos', () => {
 		});
 	});
 
+	afterEach((done) => {
+		chai.server.close();
+		done();
+	});
+
 	// test GET route
 
 	describe('/GET todos', () => {
 		it('should make a successful connection', (done) => {
 
-			chai.request(server)
+			chai.request(server.app.listen())
 				.get('/todos')
 				.end((err, res) => {
 					res.should.have.status(200);
