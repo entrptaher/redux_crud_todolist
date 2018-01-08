f4635a37015be76f6e90bb1e2f91d4395b8d059c
NeuTrix <neutrix@brandgeneering.com>
2018-01-08 09:53:26 -0800

    pass test

--- client/src/tests/components/PriorityRadio.test.js
+++ client/src/tests/components/PriorityRadio.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PriorityRadio from '../containers/PriorityRadio';
+import PriorityRadio from '../../containers/PriorityRadio';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
