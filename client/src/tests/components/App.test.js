c5718becb62840419cff64caab211628e23a1d31
NeuTrix <neutrix@brandgeneering.com>
2018-01-08 09:57:53 -0800

    pass test

--- client/src/tests/components/App.test.js
+++ client/src/tests/components/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import App from '../../components/App';
 
 import { Provider } from 'react-redux';
-import store from '../store/store';
+import store from '../../store/store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
