31feec0d4ce7fd201bb85a6bedcd5b2cec6a2713
NeuTrix <neutrix@brandgeneering.com>
2018-01-09 00:01:42 -0800

    Create ADD_TODO action test

--- client/src/tests/actionTests/AddTodo.action.test.js
+++ client/src/tests/actionTests/AddTodo.action.test.js
@@ -1,59 +1,17 @@
-/* eslint-env node, mocha */
+/* eslint-env node, mocha, chai, jest */
 
-// should split this into a reducer test suite
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import deepFreeze from 'deep-freeze';
-import { expect } from 'chai';
-import todos from '../../reducers/todoReducer';
-import TodoList from '../../containers/TodoList';
 import store from '../../store/store';
+import { addTodo } from '../../actions/todoActions';
 
-import { 
-	addTodo, 
-} from '../../actions/todoActions';
+import chai from 'chai';
+import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
 
-// ======== esllint
-/*global TodoList describe:true*/
-/*global TodoList it:true*/
-/*eslint no-undef: 'error'*/
-
-// ====================================
+// ========= 
 
 describe('The todos tasks', () => {
 
-	let state_start =[ ];
-	// freeze object to ensure immutable result
-	deepFreeze (state_start);
-		
-	let task = 'Get groceries';
-	let task2 = 'Add a second task';
-	let rank = 'High';
-	// let date = '2020-12-31';
-
-	let action = addTodo(task, rank );
-	// console.log('action => ', action)
-	let action2 = addTodo(task2, rank );
-	// console.log('action2 => ',action2)
-		
-	let list01 = todos(state_start, action );
-	// console.log('list01 => ', list01);
-	let list02 = todos(list01, action2);
-	// console.log('list02 => ', list02);
-
-	// this may want a Prover to work
-	describe('renders without crashing', () => {
-		const div = document.createElement('div');
-		ReactDOM.render(
-			<TodoList />
-			,div
-		);
-	});
- 
-	describe ('the Redux Store implementation', () => {
-
-		// console.log ('**** This is the state of the store ****', store.getState());
+	describe ('the ADD_TODO action ', () => {
 
 		const _mock ={
 			completed:  false,
@@ -64,74 +22,60 @@ describe('The todos tasks', () => {
 			task: 'Make it so'
 		};
 			
-		let _task, afterList;
-		let beforeList = (store.getState().todos);
+		let prevState = (store.getState().todos);
+			deepFreeze (prevState);
+		let _task, nextState;
+
+		// ========= 
 
 		beforeAll(() => {
 			store.dispatch(addTodo(_mock));
-			afterList = (store.getState().todos);
-			let _size = afterList.length;
-			_task = afterList[ _size - 1 ];
+			nextState = (store.getState().todos);
+				let _size = nextState.length;
+				_task = nextState[ _size - 1 ];
 		});
 
 		afterAll(() => {
-			afterList.pop();
+			nextState.pop();
 		});
 
-		it('can use ADD_TODO to add a todo to the array', () => {
+		// ========= 
+
+		it('_ _ adds a todo to the array', () => {
 			expect(_task).to.be.an('object');
-			expect(afterList.length).to.be.above(beforeList.length);
+			expect(nextState.length).to.be.above(prevState.length);
 		});
 
-		it('has a "completed" property', () => {
+		it('_ _ has a "completed" property', () => {
 			expect(_task).to.have.property('completed')
 				.to.eql(_mock.completed);
 		});
 		
-		it('has a "date" property', () => {
+		it('_ _ has a "date" property', () => {
 			expect(_task).to.have.property('date')
 				.to.eql(_mock.date);
 		});
 		
-		it('has a "details" property', () => {
+		it('_ _ has a "details" property', () => {
 			expect(_task).to.have.property('details')
 				.to.eql(_mock.details);
 		});
 		
-		it('has a "owner" property', () => {
+		it('_ _ has a "owner" property', () => {
 			expect(_task).to.have.property('owner')
 				.to.eql(_mock.owner);
 		});
 		
-		it('has a "rank" property', () => {
+		it('_ _ has a "rank" property', () => {
 			expect(_task).to.have.property('rank')
 				.to.eql(_mock.rank);
 		});
 		
-		it('has a "task" property', () => {
+		it('_ _ has a "task" property', () => {
 			expect(_task).to.have.property('task')
 				.to.eql(_mock.task);
 		});
 
 	});
-
-	describe('The ADD_TODO action ', () => {
-
-		it('...returns a redux action object', () => {
-			expect(action).to.be.an('object');
-			expect(action.payload.task).to.equal(task);
-			expect(action.payload.rank).to.equal(rank);
-		});
-
-		it('...Adds a new task through the reducer', () => {
-			expect(list01).to.be.an('array');
-			expect(list01.length).to.equal(1);
-		});
-
-		it('...Adds a second task through the reducer', () => {
-			expect(list02).to.be.an('array');
-			expect(list02.length).to.equal(2);
-		});
-	}); // descr ADD_TODO
 
 }); //master describe
