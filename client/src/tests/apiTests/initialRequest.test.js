a5643a658b4ba49a13f346eb854ead8c43bc9e4e
NeuTrix <neutrix@brandgeneering.com>
2018-01-09 00:40:36 -0800

    Update the initialRequest api test in client

--- client/src/tests/apiTests/initialRequest.test.js
+++ client/src/tests/apiTests/initialRequest.test.js
@@ -1,7 +1,6 @@
 /* eslint-env node, mocha, chai, jest */
 
 // import deepFreeze from 'deep-freeze';
-// import chai from 'chai';
 
 import { expect } from 'chai';
 import axios from 'axios';
@@ -23,42 +22,41 @@ describe('The initialRequest actions', () => {
 				_task = todos[_last];
 				done();
 			});
-
 	});
-	describe('the todo properties', () => {
 
-	it('..returns an array of objects', () => {
-		expect(todos).to.be.an('array');
-		expect(_task).to.be.an('object');
-		expect(todos.length).to.be.above(0);
+	describe('the retrieved todo items ', () => {
+
+		it('_ _ are an array of objects', () => {
 			console.log(_task);
-	});
+			expect(todos).to.be.an('array');
+			expect(_task).to.be.an('object');
+			expect(todos.length).to.be.above(0);
+		});
 
-		it('_ _ has a "completed" property', () => {
+		it('_ _ have a "completed" property', () => {
 			expect(_task).to.have.property('completed');
 		});
 				
-		it('_ _ has a "date" property', () => {
+		it('_ _ have a "date" property', () => {
 			expect(_task).to.have.property('date');
 		});
 				
-		it('_ _ has a "details" property', () => {
+		it('_ _ have a "details" property', () => {
 			expect(_task).to.have.property('details');
 		});
 				
-		it('_ _ has a "owner" property', () => {
+		it('_ _ have a "owner" property', () => {
 			expect(_task).to.have.property('owner');
 		});
 				
-		it('_ _ has a "rank" property', () => {
+		it('_ _ have a "rank" property', () => {
 			expect(_task).to.have.property('rank');
 		});
 				
-		it('_ _ has a "task" property', () => {
+		it('_ _ have a "task" property', () => {
 			expect(_task).to.have.property('task');
 		});
 		
 	});
-	
 
 });
